d18732a37d6267567e66588e44b37a6612c8633b
Curtis Joyner <123451213+CJoyner42@users.noreply.github.com>
2023-12-19 07:45:30 -0500

    refactoring

--- bcg_1.0.cpp
+++ bcg_1.0.cpp
@@ -6,87 +6,89 @@
 #include <random>
 #include <iomanip>
 
-// 1define a constant for the history file location using the USERPROFILE environment variable
-const std::string HISTORY_FILE = std::getenv("USERPROFILE") + std::string("\\Documents\\My Games\\binary calculator\\answer_history.txt");
+using namespace std;
+
+// define a constant for the history file location using the USERPROFILE environment variable
+const string HISTORY_FILE = getenv("USERPROFILE") + string("\\Documents\\My Games\\binary calculator\\answer_history.txt");
 
 // collect integer input from the user
-int collectIntegerInput(const std::string &prompt)
+int collect_integer_input(const string &prompt)
 {
     int input;
     while (true)
     {
-        std::cout << prompt;
-        if (std::cin >> input)
+        cout << prompt;
+        if (cin >> input)
         {
             break;
         }
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cerr << "\n*numbers only*" << std::endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cerr << "\n*numbers only*" << endl;
     }
     return input;
 }
 
 // collect the user's chosen difficulty
-int collectDifficultyInput()
+int collect_difficulty_input()
 {
-    std::cout << "\n-'- { difficulty } -'-\n";
-    std::cout << "\n[1] easy\n";
-    std::cout << "-\n";
-    std::cout << "[2] medium\n";
-    std::cout << "-\n";
-    std::cout << "[3] ???\n";
-    std::cout << "-\n";
-    std::cout << "[4] main menu\n";
+    cout << "\n-'- { difficulty } -'-\n";
+    cout << "\n[1] easy\n";
+    cout << "-\n";
+    cout << "[2] medium\n";
+    cout << "-\n";
+    cout << "[3] ???\n";
+    cout << "-\n";
+    cout << "[4] main menu\n";
 
     int choice;
     while (true)
     {
-        choice = collectIntegerInput("\nchoose [1 - 4]: ");
+        choice = collect_integer_input("\nchoose [1 - 4]: ");
         if (choice >= 1 && choice <= 4)
         {
             break;
         }
         else
         {
-            std::cerr << "\n*invalid selection*" << std::endl;
+            cerr << "\n*invalid selection*" << endl;
         }
     }
     return choice;
 }
 
 // display the history of correct answers
-void displayHistory()
+void display_history()
 {
-    std::ifstream historyFile(HISTORY_FILE);
-    if (!historyFile)
+    ifstream history_file(HISTORY_FILE);
+    if (!history_file)
     {
-        std::cout << "*history is empty*" << std::endl;
+        cout << "*history is empty*" << endl;
         return;
     }
 
-    std::string line;
-    std::cout << "\n-'- { correct answers } -'-\n";
-    std::cout << "\n";
-    while (std::getline(historyFile, line))
+    string line;
+    cout << "\n-'- { correct answers } -'-\n";
+    cout << "\n";
+    while (getline(history_file, line))
     {
-        std::cout << line << std::endl;
+        cout << line << endl;
     }
-    historyFile.close();
+    history_file.close();
 }
 
 // save the answer data to the history file
-void saveAnswer(const std::string &answerData)
+void save_answer(const string &answer_data)
 {
-    std::ofstream historyFile(HISTORY_FILE, std::ios::app);
-    if (historyFile)
+    ofstream history_file(HISTORY_FILE, ios::app);
+    if (history_file)
     {
-        historyFile << answerData << '\n';
-        historyFile.close();
+        history_file << answer_data << '\n';
+        history_file.close();
     }
     else
     {
-        std::cerr << "*error saving answer*" << std::endl;
+        cerr << "*error saving answer*" << endl;
     }
 }
 
@@ -94,33 +96,33 @@ void saveAnswer(const std::string &answerData)
 void play()
 {
     // collect the user's chosen difficulty
-    int selection = collectDifficultyInput();
+    int selection = collect_difficulty_input();
 
     // initialize random number generation
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist;
 
     // begin game loop
     while (true)
     {
-        int decimalRepresentation;
+        int decimal_representation;
 
         // generate a random number based on the selected difficulty
         if (selection == 1)
         {
-            decimalRepresentation = dist(gen) % 100 + 1;
-            std::cout << "\n'" << std::bitset<7>(decimalRepresentation) << "' is the binary representation of a random number between 1 and 100." << std::endl;
+            decimal_representation = dist(gen) % 100 + 1;
+            cout << "\n'" << bitset<7>(decimal_representation) << "' is the binary representation of a random number between 1 and 100." << endl;
         }
         else if (selection == 2)
         {
-            decimalRepresentation = dist(gen) % 10000 + 1;
-            std::cout << "\n'" << std::bitset<14>(decimalRepresentation) << "' is the binary representation of a random number between 1 and 10,000." << std::endl;
+            decimal_representation = dist(gen) % 10000 + 1;
+            cout << "\n'" << bitset<14>(decimal_representation) << "' is the binary representation of a random number between 1 and 10,000." << endl;
         }
         else if (selection == 3)
         {
-            decimalRepresentation = dist(gen) % 1000000 + 1;
-            std::cout << "\n'" << std::bitset<20>(decimalRepresentation) << "' is the binary representation of a random number between 1 and 1,000,000." << std::endl;
+            decimal_representation = dist(gen) % 1000000 + 1;
+            cout << "\n'" << bitset<20>(decimal_representation) << "' is the binary representation of a random number between 1 and 1,000,000." << endl;
         }
         else if (selection == 4)
         {
@@ -129,45 +131,45 @@ void play()
         }
 
         // mark the start time for measuring the user's response
-        time_t startTime = time(nullptr);
+        time_t start_time = time(nullptr);
 
         // prompt the user for their answer
-        int answer = collectIntegerInput("\nwhat is the decimal representation?: ");
+        int answer = collect_integer_input("\nwhat is the decimal representation?: ");
 
         // check if the user's answer is correct
-        if (answer == decimalRepresentation)
+        if (answer == decimal_representation)
         {
-            std::cout << "\n*correct. the answer is " << decimalRepresentation << '*' << std::endl;
+            cout << "\n*correct. the answer is " << decimal_representation << '*' << endl;
 
             // mark the end time and calculate elapsed time
-            time_t endTime = time(nullptr);
-            int elapsedSeconds = static_cast<int>(endTime - startTime);
-            int minutes = elapsedSeconds / 60;
-            int seconds = elapsedSeconds % 60;
+            time_t end_time = time(nullptr);
+            int elapsed_seconds = static_cast<int>(end_time - start_time);
+            int minutes = elapsed_seconds / 60;
+            int seconds = elapsed_seconds % 60;
 
             // format the current time and elapsed time
-            std::string currentTime = std::asctime(std::localtime(&endTime));
-            currentTime.pop_back();
+            string current_time = asctime(localtime(&end_time));
+            current_time.pop_back();
 
-            std::stringstream formattedTime;
-            formattedTime << std::put_time(std::localtime(&endTime), "%b %d, %Y {%H:%M:%S}");
+            stringstream formatted_time;
+            formatted_time << put_time(localtime(&end_time), "%b %d, %Y {%H:%M:%S}");
 
-            std::stringstream timeTaken;
-            timeTaken << std::setfill('0') << std::setw(2) << minutes << ":" << std::setfill('0') << std::setw(2) << seconds;
+            stringstream time_taken;
+            time_taken << setfill('0') << setw(2) << minutes << ":" << setfill('0') << setw(2) << seconds;
 
             // convert the decimal representation to binary and remove leading zeros
-            std::string binaryRep = std::bitset<20>(decimalRepresentation).to_string();
+            string binaryRep = bitset<20>(decimal_representation).to_string();
             binaryRep.erase(0, binaryRep.find_first_not_of('0'));
 
             // construct a string containing answer data and save it to the history file
-            std::string answerData = std::to_string(answer) + " (" + binaryRep + ") - " + formattedTime.str() + ", time taken: " + timeTaken.str();
-            saveAnswer(answerData);
+            string answer_data = to_string(answer) + " (" + binaryRep + ") - " + formatted_time.str() + ", time taken: " + time_taken.str();
+            save_answer(answer_data);
             return;
         }
         else
         {
             // report correct answer to the user
-            std::cout << "\n*incorrect. the answer is " << decimalRepresentation << '*' << std::endl;
+            cout << "\n*incorrect. the answer is " << decimal_representation << '*' << endl;
             return;
         }
     }
@@ -178,14 +180,14 @@ int main()
 {
     while (true)
     {
-        std::cout << "\n-'- { main menu } -'-\n";
-        std::cout << "\n[1] play game\n";
-        std::cout << "-\n";
-        std::cout << "[2] history\n";
-        std::cout << "-\n";
-        std::cout << "[3] exit\n";
+        cout << "\n-'- { main menu } -'-\n";
+        cout << "\n[1] play game\n";
+        cout << "-\n";
+        cout << "[2] history\n";
+        cout << "-\n";
+        cout << "[3] exit\n";
 
-        int choice = collectIntegerInput("\nchoose [1 - 3]: ");
+        int choice = collect_integer_input("\nchoose [1 - 3]: ");
 
         switch (choice)
         {
@@ -193,14 +195,14 @@ int main()
             play();
             break;
         case 2:
-            displayHistory();
+            display_history();
             break;
         case 3:
-            std::cout << "\n-'- -*-  -*- -'-\n";
-            std::cout << "\n";
+            cout << "\n-'- -*-  -*- -'-\n";
+            cout << "\n";
             return 0;
         default:
-            std::cerr << "\n*invalid selection*" << std::endl;
+            cerr << "\n*invalid selection*" << endl;
         }
     }
 }
